d5b471435b20f1923a9acab729ff589020c6b1d4
ScholzovaLucie <lucka.scholz@gmail.com>
2024-02-27 14:22:19 +0100

    first commit

---
+++ src/test/java/org/example/cv_03/Cv03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cv_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
